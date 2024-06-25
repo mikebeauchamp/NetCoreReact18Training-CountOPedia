544a8fc99f60ce87323e049607b81fdd6f8ead36
Mike Beauchamp <mbeauchamp@minitab.com>
2024-06-25 16:38:09 -0400

    2024.06.24: Added state to counter component

--- src/counter.jsx
+++ src/counter.jsx
@@ -1,18 +1,29 @@
 import React from "react";
 
 class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleAttack = this.handleAttack.bind(this);
+    this.handleDefense = this.handleDefense.bind(this);
+    this.state = {
+      count: 0,
+    };
+  }
+
   handleAttack() {
-    alert("Attack clicked");
+    //alert("Attack clicked");
+    this.setState({ count: this.state.count + 1 });
   }
 
   handleDefense() {
-    alert("Defend clicked");
+    //alert("Defend clicked");
+    this.setState({ count: this.state.count - 1 });
   }
 
   render() {
     return (
       <div className="row text-white">
-        <h1>Counter: </h1>
+        <h1>Counter: {this.state.count} </h1>
         <button onClick={this.handleAttack} style={{ width: "200px" }}>
           +1
         </button>
